9081cdfea0d4be407015e2f68645c26bfe97b6bb
Ryota2023 <hi_telecom1@yahoo.co.jp>
2024-07-16 05:33:23 +0900

    (更新) .gitignore

---
+++ data_test/data.js
@@ -0,0 +1,8 @@
+[
+    {
+    "_id": "h12ulAfEEIi1",
+    "content": "Focusing your life solely on making a buck shows a poverty of ambition. It asks too little of yourself. And it will leave you unfulfilled.",
+    "author": "Barack Obama",
+    "tags": ["Success", "Famous Quotes"]
+  }
+]
