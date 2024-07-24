6fcf5def776c1f177c423a27c7bf66492e1d1227
Ryota2023 <hi_telecom1@yahoo.co.jp>
2024-07-25 07:20:06 +0900

    更新

--- public/script/script.js
+++ public/script/script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 本番環境用
 	//   fetch('https://xs278795.xsrv.jp/study_api_famous_quotes/quote')
 	// 開発環境用
-	  fetch('/quote')
+	  fetch('./quote')
 		.then(response => {
 		  if (response.ok) {
 			return response.json();
