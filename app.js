28d63d03718ba1961295aa4944c229be8f21f9f1
Ryota2023 <hi_telecom1@yahoo.co.jp>
2024-07-26 08:07:52 +0900

    更新

--- app.js
+++ app.js
@@ -1,50 +1,37 @@
 //
-// 開発環境では、101行目のfetch関数を使う
-// 本番環境では、99行目のfetch関数を使う
+// 開発環境では、62行目のfetch関数を使う
+// 本番環境では、64行目のfetch関数を使う
 //
-require('dotenv').config();  // .envファイル読込みに必要
-
+require('dotenv').config();  // .envファイル読込み用
 const express = require('express');
 const fetch = require('node-fetch');
 const path = require('path');
 const app = express();
-const morgan = require('morgan');  // ecosystem.config.jsファイル読込みに必要
-const helmet = require('helmet');  // セキュリティヘッダーの設定
-const logger = require('./logger');  //console.logをlogs内に保存
+const morgan = require('morgan');  // ecosystem.config.jsファイル読込み用
+const helmet = require('helmet');  // セキュリティ対策
+const logger = require('./logger');  //console.logをlogs内に保存する
 
 // .envから環境変数を取得
 const apiUrl = process.env.API_URL;
-const PORT = process.env.PORT || 3100; // PORT環境変数が存在しない場合はデフォルト値を使用
+const PORT = process.env.PORT || 3100; // PORT環境変数が存在しない場合は
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // logs内にconsole.logを保存
 console.log(`PORT: ${PORT}`);
 console.log(`NODE_ENV: ${NODE_ENV}`);
 console.log(`API_URL: ${apiUrl}`);
-logger.info('●app.jsに入りました(app.js:21)');
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet());   //セキュリティ対策
 
 if (NODE_ENV === 'development') {
-   console.log('開発環境：');
    // 開発環境用
+   logger.info('●開発環境に入りました！(app.js:29行)');
    app.use('/', express.static(path.join(__dirname, 'public')));
-   app.use(morgan('dev'));
-   logger.info('●開発環境スタート！(app.js)');
-
-   // ルートパスに対するファイル提供
-   app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
-         if (err) {
-            console.log('エラー：/index.html→ ', err);
-            logger.error('エラー：/index.html→ ', err);
-         }
-      });
-   });
+   app.use(morgan('dev')); //Vscodeコンソールにログ出力
 
    app.use((req, res, next) => {
-      console.log(`Received request for: ${req.url}`);
+      console.log(`req.urlは→ ${req.url}`);
       next();
    });
 
@@ -54,29 +41,18 @@ if (NODE_ENV === 'development') {
    });
 
 } else if (NODE_ENV === 'production') {
-   console.log('本番環境用に入りました！(app.js)');
-   logger.info('本番環境用に入りました！(app.js)');
    // 本番環境用
+   logger.info('●本番環境用に入りました！(app.js:4行)');
    app.use('/study_api_famous_quotes', express.static(path.join(__dirname, 'public')));
    app.use(morgan('combined'));  // リクエストログを記録
-   app.use(helmet());  //本番環境ではセキュリティーヘッダーを使う
-
-   // トップ画面のエンドポイントを修正
-   app.get('/12345', (req, res) => {
-      res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
-         if (err) {
-            logger.error('Error sending index.html:', err);
-         }
-      });
-   });
 
    app.use((req, res, next) => {
-      logger.info(`●app.use(req,res,next)に入りました！(app.js) ${req.url}`);
+      logger.info(`●res.urlは(app.js:51行)→ ${req.url}`);
       next();
    });
 
    app.use((err, req, res, next) => {
-      logger.info('app.use((err, req, res, next)にはいりました！(app.js)');
+      logger.info('app.use((err, req, res, next)にはいりました(app.js:56行)');
       logger.error(err.stack);
       res.status(500).send('An error occurred. Please try again later.');
    });
@@ -86,7 +62,7 @@ if (NODE_ENV === 'development') {
 app.get('/study_api_famous_quotes/quote', async (req, res) => {
 // fetch関数（開発環境用）
 // app.get('/quote', async (req, res) => {
-   logger.info('●app.get("/quote")に入りました！(app.js)');   //デバッグ用
+   logger.info('●fetch関数内に入りました！(app.js:65行)');   //デバッグ用
 
    try {
       const response = await fetch('https://raw.githubusercontent.com/quotable-io/data/master/data/quotes.json');
@@ -110,5 +86,5 @@ app.use((req, res, next) => {
 });
 
 app.listen(PORT, () => {
-   console.log(`Server is running → http://localhost:${PORT}`);
+   console.log(`●Server is running → http://localhost:${PORT}`);
 });
