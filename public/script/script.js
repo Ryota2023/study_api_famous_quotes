8e58c0b9468bde4d882cac5a7c29898c647a4b4c
Ryota2023 <hi_telecom1@yahoo.co.jp>
2024-07-25 10:51:20 +0900

    更新

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,3 @@
-let count = false;
 document.addEventListener('DOMContentLoaded', () => {
     const clickSound = document.getElementById('soundEffect');
     if (clickSound) {
@@ -10,44 +9,43 @@ document.addEventListener('DOMContentLoaded', () => {
         }, false);
     }
 
-    document.getElementById('getQuote').addEventListener('click', async () => {
-        try {
-            const soundForm = document.getElementById('soundForm');
-            const selectedRadio = soundForm.querySelector('input[name="radio"]:checked');
+    document.getElementById('getQuote').addEventListener('click', () => {
+        const soundForm = document.getElementById('soundForm');
+        const selectedRadio = soundForm.querySelector('input[name="radio"]:checked');
 
-            if (selectedRadio && selectedRadio.value === 'ON') {
-                if (clickSound) {
-                    await clickSound.play().catch(error => {
-                        console.error('Error playing the sound:', error);
-                    });
-                }
+        if (selectedRadio && selectedRadio.value === 'ON') {
+            if (clickSound) {
+                clickSound.play().catch(error => {
+                    console.error('Error playing the sound:', error);
+                });
             }
-
-            // Fetch request
-            const response = await fetch('./quote');
-            if (!response.ok) {
-                throw new Error(`Network response was not ok: ${response.statusText}`);
-            }
-            const data = await response.json();
-            displayQuote(data);
-        } catch (error) {
-            console.error('エラー発生: fetching the quote:', error);
-            document.getElementById('quoteDisplay').innerText = 'Failed to fetch quote.';
         }
+
+        fetch('./quote')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            })
+            .then(data => {
+                const quoteE = document.getElementById('quoteDisplay');
+                const dsp1E = document.createElement('div');
+                const dsp2E = document.createElement('div');
+    
+                dsp1E.innerHTML = `●Name: ${data.author}<br><br>`;
+                dsp2E.innerHTML = `　${data.content}<br><br><hr>`;
+    
+                quoteE.appendChild(dsp1E);
+                quoteE.appendChild(dsp2E);
+    
+                dsp2E.scrollIntoView({ behavior: 'smooth' });
+            })
+            .catch(error => {
+                console.error('エラー発生: fetching the quote:', error);
+                document.getElementById('quoteDisplay').innerText = 'Failed to fetch quote.';
+            });
     });
-
-    function displayQuote(data) {
-        const quoteE = document.getElementById('quoteDisplay');
-        const dsp1E = document.createElement('div');
-        const dsp2E = document.createElement('div');
-        dsp1E.innerHTML = `●Name: ${data.author}<br><br>`;
-        dsp2E.innerHTML = `　${data.content}<br><br><hr>`;
-
-        quoteE.appendChild(dsp1E);
-        quoteE.appendChild(dsp2E);
-
-        dsp2E.scrollIntoView({ behavior: 'smooth' });
-    }
 
     const soundForm = document.getElementById('soundForm');
     soundForm.addEventListener('change', (event) => {
